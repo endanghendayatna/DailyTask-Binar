eef52e35717aee6bccb86353f1917f2c95bf546f
Endang Hendayatna <91519488+endanghendayatna@users.noreply.github.com>
2023-03-15 18:19:36 +0700

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,478 @@
+const person = [
+  {
+    "_id": "64107c1257e5858a1e47e854",
+    "age": 34,
+    "eyeColor": "blue",
+    "name": "Leon Hogan",
+    "address": "Bali",
+    "registered": "2020-09-16T09:47:53 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Theresia",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "baseball"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Theresia",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c129f9fa9bf6d63046d",
+    "age": 26,
+    "eyeColor": "blue",
+    "name": "Willis Daniels",
+    "address": "Bali",
+    "registered": "2020-07-10T07:53:01 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 2,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Farid",
+        "isActive": true,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "basketball"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c1255668b2993324741",
+    "age": 23,
+    "eyeColor": "brown",
+    "name": "Robert Clements",
+    "address": "Bali",
+    "registered": "2022-10-26T05:57:49 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Farid",
+        "isActive": true,
+        "favoriteFruit": "apple",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c128173410d0ab67f2c",
+    "age": 37,
+    "eyeColor": "blue",
+    "name": "Carole Madden",
+    "address": "Jakarta",
+    "registered": "2022-02-08T06:05:50 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Theresia",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "football"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Theresia",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c121cdebe53c78fc81b",
+    "age": 23,
+    "eyeColor": "green",
+    "name": "Fowler Dodson",
+    "address": "Bandung",
+    "registered": "2021-08-29T01:13:13 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Theresia",
+        "isActive": false,
+        "favoriteFruit": "strawberry",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 2,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c12f105d0413941117a",
+    "age": 28,
+    "eyeColor": "green",
+    "name": "Green Andrews",
+    "address": "Bali",
+    "registered": "2020-02-16T10:59:55 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Farid",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "baseball"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "banana",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "apple",
+        "gender": "male",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 2,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "_id": "64107c123dae7f595701cb6d",
+    "age": 24,
+    "eyeColor": "blue",
+    "name": "Kelli Franks",
+    "address": "Bali",
+    "registered": "2022-06-17T03:32:41 -07:00",
+    "friends": [
+      {
+        "id": 0,
+        "name": "Shifly",
+        "isActive": false,
+        "favoriteFruit": "banana",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 1,
+            "hobby": "football"
+          },
+          {
+            "id": 2,
+            "hobby": "basketball"
+          }
+        ]
+      },
+      {
+        "id": 1,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "basketball"
+          },
+          {
+            "id": 1,
+            "hobby": "baseball"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "name": "Shifly",
+        "isActive": true,
+        "favoriteFruit": "strawberry",
+        "gender": "female",
+        "hobby": [
+          {
+            "id": 2,
+            "hobby": "football"
+          },
+          {
+            "id": 1,
+            "hobby": "football"
+          }
+        ]
+      }
+    ]
+  }
+]
+
+// 1) display/print person yang registered dibawah tahun 2022
+console.log(data.find(element => {
+    return parseInt(element.registered.substring(0, 4)) < 2022
+}))
+
+// 2) display person yang address nya Bali
+console.log(person.find(Element => {
+  return Element.address === 'Bali'
+})) 
+
+
+// // 3) display friends yang hobby nya football
+
+
+// // 4) display hobby dari friends id 2
+console.log(
+    person.map((a) => {
+        return a.friends.filter(b => {
+            return b.id === 2
+        })
+    }))
+
+// 5) display friends yang gender nya male dan mempunya hobby basketball
+
+
+// 6) display friends yang isActive nya true dan gender nya female dan favorite fruit nya strawberry
+const filteredPerson = person.filter(persons => { 
+  return persons.friends.some(friend => friend.isActive === true && friend.gender === "female" && friend.favoriteFruit === "strawberry")
+})
+console.log(filteredPerson)
+
+// 7) display siapa saja yang mempunya teman name Theresia
+
+
+// 8) display hobby id 1 dari friends yang isActive nya true
+let hobby = person.map(x => {
+  return x.friends.filter(y => {
+    return y.isActive === true && y.hobby.filter(z => z.id === 1)
+  })
+})
+console.log(hobby);
+
+
+// 9) display person yang eye color nya brown dan favorite fruit nya banana dan mempunyai teman hobby basketball
+
+
+// 10) display person yang mempunya friends gender male atau favorite fruit nya banana dan registered di tahun 2023
+
